c101a63830788e5b39301181e64faf040d08ff29
Anthony Rossiter <j.a.rossiter@sheffield.ac.uk>
2023-07-04 11:40:19 +0100

    update july 5th

---
+++ control education and virtual laboratories/Contents.m
@@ -0,0 +1,58 @@
+%
+%            Control 101 toolbox files
+% 
+%  For a more detailed overview, see the website:
+%  https://controleducation.sites.sheffield.ac.uk/matlabresources/community-control-toolbox
+%
+% Virtual Laboratories/MATLAB apps: Assuming the toolbox has been installed,
+% the apps should appear under the apps tab so you can open from there.
+%
+%   aeroplanelanding_behaviour
+%   aeroplane_roll_control
+%   carmodel_and_behaviour     
+%   cruisecontrol                                      
+%   dcservo_and_position_control                       
+%   firstordermodel_behaviour                       
+%   firstordermodels_response_characterisation 
+%   housetemperature_and_behaviour                     
+%   invertedpendulum         
+%   mixingtank_and_behaviour                           
+%   tanklevel_and_PIcontrol                            
+%   tanklevelbehaviour
+%   behaviours
+%   pi_tuning
+%
+%
+% Livescript files for learning control basics alongside simple MATLAB code
+% for analysis and evaluation. These files open a duplicate livescript file
+% in the pwd directly so the user can make edits and save these.
+%
+%   ODEs_with_matlab                                          
+%   bode_asymptotes_toolbox                                                         
+%   closed_loop_compare_multiple_compensators_toolbox         
+%   closed_vs_openloop_overlay                                
+%   closedloop_offset_and_poles                               
+%   closedloop_transferfunctions_with_feedback                
+%   delays_and_bode                                                                          
+%   discrete_models_and_usage                                 
+%   discretisation_and_bode                                   
+%   firstordermodels_in_matlab                                
+%   firstordermodels_responses_analysis_and_feedback          
+%   freq_responses_with_matlab                                
+%   lag_design_with_bode                                      
+%   laplace_transforms_with_matlab                            
+%   lead_design_with_bode                                     
+%   lead_lag_design_with_bode                                 
+%   partial_fractions_with_matlab                             
+%   pi_tuning_manual                                          
+%   plotting_with_matlab                                      
+%   proportional_design_with_bode                             
+%   secondordermodels_and_underdamping                        
+%   secondordermodels_in_matlab                               
+%   step_responses_with_matlab                                
+%   time_series_models                                        
+%   time_series_models_and_recursion                          
+%   transferfunctions_and_behaviours                          
+%   transferfunctions_and_poles                               
+
+
---
+++ control education and virtual laboratories/README.md
@@ -0,0 +1,17 @@
+# control101ver2
+MATLAB toolbox for a Control 101 course
+
+The community toolbox project is led by Anthony Rossiter with sponsorship from MathWorks  (under project "A MATLAB community toolbox to support learning of Control 101") and input from numerous international colleagues. The intention is that the resources/toolbox developed here will be available from within MATLAB using the "add-ons" button. The resources are shared under a BSD license so users can edit, adopt and use freely.
+
+A summary is given below for completeness; more detailed information is within each individual resource. Some more overview information is on the authors website: https://controleducation.sites.sheffield.ac.uk/matlabresources/community-control-toolbox
+
+Several people have developed MATLAB based materials to support the teaching of “Control Systems 101” type courses; the particular focus here is on interactive and animated resources which are largely self-explanatory, that is easy to use,  and support student understanding and learning. They demonstrate authentic scenarios across a broad range of topics which motivate the importance of control to modern society, alongside some technical learning outcomes to support university curricula.
+
+Resources are divided into:
+1) Virtual laboratories which have simple GUI interfaces so students can select buttons and sliders to observe the behaviour of some authentic scenarios and see how different parameter choices affect behaviour. Each virtual laboratory comprises two files which run independently: 
+i) the mlapp file which is the laboratory itself and ii) a partner livescript file which acts like a concise manual giving context, illustrative MATLAB source code and some technical background.  Naming conventions are largely self-evident. For example, a typical naming convention for the paired app files is: mixingtank_and_behaviour.mlapp & mixingtank_and_behaviour_manual.mlx. 
+
+2) Livescript files which focus more on useful MATLAB code students can use to create their own files to support detailed analysis, evaluation and design, as required for university courses. These files are  summarised in section 6.9.
+
+Cite As
+John Anthony Rossiter (2023). Control 101 toolbox (https://github.com/jarossiter/control101ver1/releases/tag/v0.1), GitHub. Retrieved June 29, 2023.
