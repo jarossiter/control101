9aa696579f90f8487c6e6e3bae8104b67f0504d1
unknown <j.a.rossiter@sheffield.ac.uk>
2024-03-28 07:44:10 +0000

    minor error in help doc corrected

--- control101/livescripts for firstcourse in control/matlabCode/Youla_continuous_controller_design.m
+++ control101/livescripts for firstcourse in control/matlabCode/Youla_continuous_controller_design.m
@@ -8,7 +8,7 @@ if exist('Youla_continuous_controller_design_toolbox_mycopy.mlx','file')
     disp('Opening existing copy - delete or rename this copy to access toolbox orginal')
     open Youla_continuous_controller_design_toolbox_mycopy.mlx
 else
-p1 = which('Youla_continuous_controller_design_toolbox_toolbox.mlx');
+p1 = which('Youla_continuous_controller_design_toolbox.mlx');
 p2 = strcat(pwd,'/Youla_continuous_controller_design_toolbox_mycopy.mlx');
 copyfile(p1,p2)
 open Youla_continuous_controller_design_toolbox_mycopy.mlx
